166c966348affda82489ef0b2eb72d15d373b190
unlogisch04 <98281608+unlogisch04@users.noreply.github.com>
2026-01-13 23:34:03 +0100

    Add more Infos and use only flash for text

--- src/init.h
+++ src/init.h
@@ -25,6 +25,9 @@
 #ifdef ESP8266
 #include <user_interface.h>
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
 typedef struct rtc_mem {
 	uint32_t version;  // RTC memory version
 	uint32_t rebootCount;  // Number of reboots
@@ -35,19 +38,29 @@ extern "C" void preinit(void) {
 	struct rst_info* resetreason;
 	rtc_mem_t rtcMem;
 	resetreason = ESP.getResetInfoPtr();
-	Serialtemp.begin(115200);
-
-	Serialtemp.println("\r\n==== SLVR Boot ====");
-	Serialtemp.println("Reboot reason code: " + String(ESP.getResetInfoPtr()->reason));
-	Serialtemp.println("Core Version: " + ESP.getCoreVersion());
-	Serialtemp.println("SDK version: " + String(ESP.getSdkVersion()));
-	Serialtemp.println("Sketch MD5: " + String(ESP.getSketchMD5()));
-	Serialtemp.println("Reset reason code: " + String(resetreason->reason));
-
 	// Offset 33 to avoid eboot command area
 	ESP.rtcUserMemoryRead(33, (uint32_t*)&rtcMem, sizeof(struct rtc_mem));
-	Serialtemp.println("RTC Memory Version: " + String(rtcMem.version));
-	Serialtemp.println("RTC Memory Reboot Count: " + String(rtcMem.rebootCount));
+	Serialtemp.begin(115200);
+	Serialtemp.println(F("\r\n==== SLVR Boot ===="));
+	Serialtemp.println(F("Reboot reason code: ") + String(ESP.getResetInfoPtr()->reason));
+	Serialtemp.println(F("Core Version: ") + ESP.getCoreVersion());
+	Serialtemp.println(F("SDK version: ") + String(ESP.getSdkVersion()));
+	Serialtemp.println(F("Sketch MD5: ") + String(ESP.getSketchMD5()));
+	Serialtemp.println(F("Reset reason code: ") + String(resetreason->reason));
+	Serialtemp.println(F("RTC Memory Version: ") + String(rtcMem.version));
+	Serialtemp.println(F("RTC Memory Reboot Count: ") + String(rtcMem.rebootCount));
+	Serialtemp.println("");
+	Serialtemp.println(F("PRODUCT_NAME: ") + String(PRODUCT_NAME));
+	Serialtemp.println(F("VENDOR_NAME: ") + String(VENDOR_NAME));
+	Serialtemp.println(F("VENDOR_URL: ") + String(VENDOR_URL));
+	Serialtemp.println(F("Firmware update URL: ") + String(UPDATE_ADDRESS));
+	Serialtemp.println(F("BOARD: ") + String(BOARD));
+	Serialtemp.println(F("HARDWARE_MCU: ") + String(HARDWARE_MCU));
+	Serialtemp.println(F("PROTOCOL_VERSION: ") + String(PROTOCOL_VERSION));
+	Serialtemp.println(F("FIRMWARE_VERSION: ") + String(FIRMWARE_VERSION));
+	Serialtemp.println(F("SENSOR_DESC_LIST: "));
+	Serialtemp.println(String(TOSTRING(SENSOR_DESC_LIST)));
+
 	if (rtcMem.version != 0x01) {
 		// First boot, initialize RTC memory
 		rtcMem.version = 0x01;
@@ -65,8 +78,8 @@ extern "C" void preinit(void) {
 		// If more than 3 consecutive crashes, enter safe mode
 		if (rtcMem.rebootCount >= 3) {
 			// Boot into UART download mode
-			Serialtemp.println("\r\n\r\nEntering safe mode due to repeated crashes.");
-			Serialtemp.println("Entering flash mode...");
+			Serialtemp.println(F("\r\n\r\nEntering safe mode due to repeated crashes."));
+			Serialtemp.println(F("Entering flash mode..."));
 			// Serial needs to be stay active for
 			// rebootIntoUartDownloadMode to work.
 			// Found out over testing.
@@ -76,7 +89,7 @@ extern "C" void preinit(void) {
 	}
 	ESP.rtcUserMemoryWrite(33, (uint32_t*)&rtcMem, sizeof(struct rtc_mem));
 
-	Serialtemp.println("=== SLVR Boot end ===");
+	Serialtemp.println(F("=== SLVR Boot end ==="));
 	Serialtemp.flush();
 	// Deinit UART for main code to reinitialize
 	Serialtemp.end();
